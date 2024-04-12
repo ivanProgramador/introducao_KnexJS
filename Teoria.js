743d03375fb8a41b8076abd29f3a2e22c99ba68a
ivan <ivan-guita@hotmail.com>
2024-04-12 10:57:13 -0300

    aula de introdução ao KNEX.JS

---
+++ Teoria.js
@@ -0,0 +1,10 @@
+/*
+ Pra manipular bases de dados temos de usar SQL é uma liguagem poderosa 
+ muito bem estruturada, porém mistrurar ela com javascript no mesmo código
+ vai deixar um aparencia desorganização. Por isso quando a aplicação é escrita
+ em Js o oconselhável é não fazer isso diretamente. E sim atráves de bibliotecas 
+ proprias para esse tipo de manipulação.
+ 
+ A KNEX.JS é a bilioteca que eu vopu usar agora mas exitem muitas outras capazes de fazer 
+ isso também. 
+*/
