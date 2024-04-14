d7719405d799234ca694865d1e70652919948466
ivan <ivan-guita@hotmail.com>
2024-04-14 15:43:29 -0300

    tratando os ddados vindos do knex usando join e array fim da aula 244

--- BANCO/index.js
+++ BANCO/index.js
@@ -11,12 +11,10 @@ var knex = require('knex')({
     },
   });
 
-  knex.select(["games.*","estudios.nome as estudio_nome"])
-      .table("games")
-      .innerJoin("estudios","estudios.game_id","games.id")
-      .where("games.id",5)
-      .then(data=>{console.log(data)})
-      .catch(err=>{console.log(err)});
+
+  
+
+  
 
 
 
---
+++ BANCO/queries/tratamento_de_dados.js
@@ -0,0 +1,40 @@
+
+/*
+   Relacionamento de um para muitos 
+   Nesse caso eu vou tratar um jogo que tem varios estudios
+   eu preciso retornar o jogos e soa estudios aos quais ele pertence
+   só que nesse caso o knex traz todos os dados seu eu quiser os dados
+   de um jeito especifico eu tenho que tratar
+   */
+
+
+knex.select(["games.*","estudios.nome as estudio_nome"])
+   .table("games")
+   .innerJoin("estudios","estudios.game_id","games.id")
+   .then(data=>{
+         //essa variavel recebe os dados 
+          var estudiosGamesArray = data;
+        
+        //aqui eu crio um objeto que vai recebre o nome eo id do jogo
+        //e um sub array que vasio receber os estudios
+
+          var game ={
+            id:0,
+            nome:"",
+            estudios:[]
+          }
+         
+          game.id = data[0].id; // aqui eu coloco o id do jogo que vem dentro do array data na posição 0
+          game.nome  = data[0].nome; // aqui eu coloco o nome do jogo que vem no array data na posição 0 tambem
+          
+          //aqui eu percorro todo o array de estudios
+          data.forEach(estudio=>{
+              //e para cada estudio encontrado eu coloco no sub array de estudios  
+              estudiosGamesArray.estudios.push({nome: estudio.estudio_nome})
+          })
+          //a agora eu cosigo mostrar o jogo e os estudios soa quais ele pertence 
+          console.log(game);
+
+
+        })
+    .catch(err=>{console.log(err)});
