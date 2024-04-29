df4e561da4980ff63df334180f265382590c6388
Ivan <ivan-guita@hotmail.com>
2024-04-29 12:45:07 -0300

    Atualizar o tratamento_de_dados.js
    Correção de dados de descrição do código

--- BANCO/queries/tratamento_de_dados.js
+++ BANCO/queries/tratamento_de_dados.js
@@ -1,39 +1,44 @@
 
 /*
-   Relacionamento de um para muitos.
-     Nesse caso eu vou tratar um jogo que tem varios estudios, eu preciso retornar o jogo e sua e lista de estúdios.
 
-     só que nesse caso o knex traz todos os dados se eu quiser os dados de um jeito especifico eu tenho que tratar.
+Relacionamento de um para muitos.
+     Nesse caso eu vou tratar um jogo que tem vários estúdios, eu preciso retornar o jogo e sua e lista de estúdios.
+
+     só que nesse caso o knex traz todos os dados, mostrando uma lista de relacionamentos no terminal, se eu quiser os dados de um jeito especifico eu tenho que tratar.
+
+    A ideia principal e trazer um jogo e uma lista de estúdios que desenvolveram ele.
+
 */
 
 
-knex.select(["games.*","estudios.nome as estudio_nome"])
-   .table("games")
-   .innerJoin("estudios","estudios.game_id","games.id")
-   .then(data=>{
+knex.select(["games.*","estudios.nome as estudio_nome"]).table("games").innerJoin("estudios","estudios.game_id","games.id").then(data=>{
+
          //essa variavel recebe os dados 
           var estudiosGamesArray = data;
-        
-        //aqui eu crio um objeto que vai recebre o nome eo id do jogo
-        //e um sub array que vasio receber os estudios
+
+         //aqui eu crio um objeto que vai receber o nome eo id do jogo
+
+        //e um sub-array que vazio receber os estudios
 
           var game ={
             id:0,
             nome:"",
             estudios:[]
           }
-         
-          game.id = data[0].id; // aqui eu coloco o id do jogo que vem dentro do array data na posição 0
-          game.nome  = data[0].nome; // aqui eu coloco o nome do jogo que vem no array data na posição 0 tambem
+
+      game.id = data[0].id; // id do jogo
+      game.nome  = data[0].nome; // nome do jogo
           
-          //aqui eu percorro todo o array de estudios
-          data.forEach(estudio=>{
-              //e para cada estudio encontrado eu coloco no sub array de estudios  
+ //escaneado o array para pagar os dados 
+
+      data.forEach(estudio=>{
+
+//cada dado encontrado entra no índice dosub-array
               estudiosGamesArray.estudios.push({nome: estudio.estudio_nome})
           })
-          //a agora eu cosigo mostrar o jogo e os estudios soa quais ele pertence 
-          console.log(game);
 
+/* mostrando um objeto game com os     estúdiosresponsáveis por ele
+*/ 
+      console.log(game);
 
-        })
-    .catch(err=>{console.log(err)});
+}).catch(err=>{console.log(err)});
